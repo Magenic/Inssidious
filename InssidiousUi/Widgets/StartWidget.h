039298df2d29ae2790e3a2bb106d14cbc30bfcb4
ianbannerman <ian@internals.io>
2015-09-03 16:55:48 -0500

    Fixed a bug with hover vs pressed style sheet state

--- InssidiousUi/Widgets/StartWidget.h
+++ InssidiousUi/Widgets/StartWidget.h
@@ -81,7 +81,7 @@ private:
 	   "QPushButton{ border: 1px solid #72C55D; border-radius: 2px; background-color: #72C55D; color:#333333; font-family: 'Segoe UI Semibold'; font-size:16px; font-weight:400; text-decoration:none; }\
 		QPushButton:!enabled{ background-color: #F0F0F0; color:#444444; }\
 		QPushButton:pressed{ background-color: #64B450; color:#333333;}\
-		QPushButton:hover{ background-color: #82B678; }\
+		QPushButton:hover:!pressed{ background-color: #82B678; }\
 		QPushButton:on{ background-color: #72C55D; color:#333333;}\
 		QPushButton:focus{ outline: none; }";
 
--- InssidiousUi/Widgets/Tamper/UiTamperModule.h
+++ InssidiousUi/Widgets/Tamper/UiTamperModule.h
@@ -45,7 +45,7 @@ public:
 	   "QPushButton{ margin: 0px; padding: 0px; border: 1px solid #72C55D; border-radius: 2px; background-color: #FDFDFD; color:#444444; font-family: 'Segoe UI'; font-size:11px; font-weight:400; text-decoration:none; }\
 		QPushButton:!enabled{ border: 1px solid #ABABAB; background-color: #F0F0F0; color:#B0ADB0; }\
 		QPushButton:pressed{ background-color: #64B450; color:#333333;}\
-		QPushButton:hover{ background-color: #82B678; }\
+		QPushButton:hover:!pressed{ background-color: #82B678; }\
 		QPushButton:on{ background-color: #72C55D; color:#333333;}\
 		QPushButton:focus{ outline: none; }";
 
